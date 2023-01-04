784788e29a81be6ac0fcf61e91e44e529417baae
hyunhwa <hyunhwa@local.officeplus.com>
2023-01-04 20:42:17 +0900

    getDay

--- Algorithm/src/basic/StreamExample.java
+++ Algorithm/src/basic/StreamExample.java
@@ -8,7 +8,18 @@ import java.util.stream.Stream;
 public class StreamExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String[] info = {"java backend junior pizza 150"
+				,"python frontend senior chicken 210"
+				,"python frontend senior chicken 150"
+				,"cpp backend senior pizza 260"
+				,"java backend junior chicken 80"
+				,"python backend senior chicken 50"};
+		
+		List<String> infoList = Arrays.asList(info);
+		
+		List<String[]> infoListStream = 
+	            infoList.stream().map(list -> list.split(" ")).collect(Collectors.toList());
 
 		String[] name1 = {"Sehoon", "Songwoo", "Chan", "Youngsuk", "Dajung"};
 		List<String> name2 = Arrays.asList(name1);
@@ -22,10 +33,12 @@ public class StreamExample {
         stream3.map(name -> name.toUpperCase())
     		.forEach(name -> System.out.println(name));
         
+        System.out.println();
+        
         name2.stream()
-        .map(name -> name.toUpperCase())
         .filter(name -> name.length() >= 5)
-        .forEach(System.out::print);
+        .map(name -> name.toUpperCase())       
+        .forEach(System.out::println);
 		
         int[] number1 = {1,2,3,4,5};
         List<Integer> number2 = Arrays.asList(Arrays.stream(number1).boxed().toArray(Integer[]::new));
@@ -44,24 +57,26 @@ public class StreamExample {
         		.filter(i -> i>=3)
         		.reduce(0, (a,b) -> a + 1);
         
-        System.out.println(count);
+        System.out.println("count:"+count);
         
-        System.out.println(sum);
+        System.out.println("sum:"+sum);
         
         int max = number2.stream()
         		.filter(i -> i>=3)
+        		//.reduce(Integer.MIN_VALUE, (a,b)-> a > b ? a : b);
         		.reduce(Integer.MIN_VALUE, (a,b)-> a > b ? a : b);
         
-        System.out.println(max);
+        System.out.println("max:"+max);
         
         int min = number2.stream()
         		.filter(i -> i>=3)
         		.reduce(Integer.MAX_VALUE, (a,b)-> a < b ? a : b);
         
-        System.out.println(min);
+        System.out.println("min:"+min);
         
+        int[] scores = {1,2,3,3,5};
+        int[] array = IntStream.of(scores).distinct().toArray();
         
-
         IntStream intStream = IntStream.range(1, 5);
         Stream<Integer> istream = intStream.boxed();
         Integer[] result = istream.toArray(Integer[]::new);
@@ -71,10 +86,16 @@ public class StreamExample {
         Stream<Integer> istream2 = intStream2.boxed();
         List<Integer> list2 = istream2.collect(Collectors.toList());
         
-//        int count = intStream.reduce(0, (a,b) -> a + 1);
-//        int sum = intStream.reduce(0, (a,b) -> a + b);
-//        int max = intStream.reduce(Integer.MIN_VALUE, (a,b)-> a > b ? a : b); // max()
-//        int min = intStream.reduce(Integer.MAX_VALUE, (a,b)-> a < b ? a : b); // min()
+        IntStream intStream3 = IntStream.of(scores);
+        System.out.println(IntStream.range(1, 5).findFirst().getAsInt());
+        
+        System.out.println(number2.stream().findFirst().get());
+        
+        System.out.println(IntStream.of(scores).distinct().count());
+        System.out.println();
+        
+        IntStream.of(scores).distinct().boxed().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+        
 		
 	}
 
